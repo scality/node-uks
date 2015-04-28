7b58892ee6fddb3ed13af4b247baeeaef67711bb
Ian Hunter <beanland2@gmail.com>
2015-04-27 18:12:18 -0600

    Further stubbing-out methods

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = {
-  key: require('./lib/key')
+  key: require('./lib/key'),
+  keyrange: require('./lib/keyrange'),
+  createKeyArc: require('./lib/createKeyArc'),
+  createRandomKey: require('./lib/createRandomKey'),
+  CoS: require('./lib/cos')
 }
