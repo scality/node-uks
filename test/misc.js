696aa63770402415d7747812fed1fbf5f73cc478
Ian Hunter <beanland2@gmail.com>
2015-04-30 21:17:12 -0600

    pack754 working.

---
+++ test/misc.js
@@ -0,0 +1,26 @@
+var expect   = require('chai').expect;
+var keyutils = require('../index').keyutils;
+
+describe('keyutils', function() {
+  describe('flattenSequence', function() {
+    it('should return a flat array from an n-dimensional array', function() {
+      var ar = [1,2,[3,4],[[5],6],[[7,8],9]];
+      var expected = [1,2,3,4,5,6,7,8,9];
+      expect(keyutils.flattenSequence(ar)).to.deep.equal(expected);
+    });
+  });
+
+  describe('pack754', function() {
+    it('should return 1 when f is 0', function() {
+      expect(keyutils.pack754(0, 1, 1)).to.equal(0);
+    });
+
+    it('should return expected values', function() {
+      expect(keyutils.pack754(2, 5, 1).toString()).to.equal('8');
+      expect(keyutils.pack754(-2, 12, 11).toString()).to.equal('3072');
+      expect(keyutils.pack754(0.4, 12, 11).toString()).to.equal('1021');
+      expect(keyutils.pack754(24, 53, 11).toString()).to.equal('2259496395079680');
+    });
+
+  });
+});
